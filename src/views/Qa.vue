4c4d68c0c0e8d6a7e7b8a4cdf51766bdc73d8935
Uice Lu <uicestone@gmail.com>
2021-03-24 15:34:52 +0800

    q & a

--- src/views/Qa.vue
+++ src/views/Qa.vue
@@ -5,113 +5,93 @@ div
     .content
       .swiper-container
         .swiper-wrapper
-          .swiper-slide.swiper-no-swiping
-            .tm_num.flexCenter 第一题
-            .tm_title ____南昌南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义起义
+          .swiper-slide(
+            v-for="(question, index) in day.questions",
+            :key="question.id"
+          )
+            .tm_num.flexCenter 第{{ index + 1 }}题
+            .tm_title {{ question.title }}
             ul.opt
-              li(data-opt="a")
+              li(:class="{ current: selectedOption === index }")(
+                data-opt="a",
+                v-for="(option, index) in question.options",
+                @click="selectedOption = index"
+              )
                 i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="b")
-                i
-                span 南义南昌起义
-              li(data-opt="c")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="d")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-            .next.flexCenter 下一题
-          .swiper-slide.swiper-no-swiping
-            .tm_num.flexCenter 第二题
-            .tm_title ____南昌南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义起义
-            ul.opt
-              li(data-opt="a")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="b")
-                i
-                span 南义南昌起义
-              li(data-opt="c")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="d")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-            .next.flexCenter 下一题
-          .swiper-slide.swiper-no-swiping
-            .tm_num.flexCenter 第三题
-            .tm_title ____南昌南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义起义
-            ul.opt
-              li(data-opt="a")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="b")
-                i
-                span 南义南昌起义
-              li(data-opt="c")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="d")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-            .next.flexCenter 下一题
-          .swiper-slide.swiper-no-swiping
-            .tm_num.flexCenter 第四题
-            .tm_title ____南昌南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义起义
-            ul.opt
-              li(data-opt="a")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="b")
-                i
-                span 南义南昌起义
-              li(data-opt="c")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="d")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-            .next.flexCenter 下一题
-          .swiper-slide.swiper-no-swiping
-            .tm_num.flexCenter 第五题
-            .tm_title ____南昌南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义南昌起义起义
-            ul.opt
-              li(data-opt="a")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="b")
-                i
-                span 南义南昌起义
-              li(data-opt="c")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-              li(data-opt="d")
-                i
-                span 南昌南昌南昌起义南昌南昌昌南昌起义南昌起义
-            .tj.flexCenter 提交
-  .pop.pop1(style="display: none;")
+                span {{ option }}
+            .next.flexCenter(
+              v-if="index < day.questions.length - 1",
+              :class="{ disabled: selectedOption === null }",
+              @click="next(question)"
+            ) 下一题
+            .tj.flexCenter(
+              v-else,
+              :class="{ disabled: selectedOption === null }",
+              @click="submit(question)"
+            ) 提交
+  .pop.pop1(v-if="wrong")
     .tips
       h1 很遗憾,回答错误
       img.icon1(src="/images/6-qa-wrong.png")
       .flexBetween(style="width: 100%;padding: 0 0.5rem;margin-top: 1rem;")
-        .btn.btn1.flexCenter 返回首页
-        .btn.btn2.flexCenter 再来一次
-  answer-success
+        .btn.btn1.flexCenter(@click="$router.replace('/')") 返回首页
+        .btn.btn2.flexCenter(@click="wrong = false; selectedOption = null") 再来一次
+  answer-success(v-if="success")
 </template>
 
 <script>
+import Swiper from "swiper";
+import "swiper/css/swiper.css";
 import AnswerSuccess from "../components/AnswerSuccess";
+import { get100Day, saveAnswer } from "@/helpers/resource";
 
 export default {
   components: { AnswerSuccess },
+  data: () => {
+    return {
+      swiper: null,
+      day: {},
+      success: false,
+      wrong: false,
+      submitting: false,
+      selectedOption: null,
+    };
+  },
+  methods: {
+    next(question) {
+      if (question.trueOption !== this.selectedOption) {
+        this.wrong = true;
+        return false;
+      } else {
+        this.selectedOption = null;
+        this.swiper.slideNext();
+        return true;
+      }
+    },
+    async submit(question) {
+      if (!this.next(question)) return;
+      this.submitting = true;
+      await saveAnswer(this.$openid, this.day.id, {});
+      this.submitting = false;
+      this.success = true;
+    },
+  },
+  async created() {
+    const dayId = this.$route.query.dayId;
+    this.day = await get100Day(dayId);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.swiper = new Swiper(".swiper-container");
+    }, 1000);
+  },
 };
 </script>
 
 <style scoped>
 .main {
   width: 100%;
-  height: 21.16rem;
+  /* height: 21.16rem; */
   background: url(/images/3-bg.png) no-repeat;
   background-size: cover;
   background-position: center center;
@@ -192,6 +172,10 @@ export default {
   font-weight: bold;
   color: #956134;
   margin-top: 1rem;
+}
+.next.disabled,
+.tj.disabled {
+  opacity: 0.5;
 }
 .pop {
   width: 100%;
