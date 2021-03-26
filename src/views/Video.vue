be747d9f467e93d73f85c2f0a795fd9efefc79b7
Uice Lu <uicestone@gmail.com>
2021-03-27 00:10:12 +0800

    video answer

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,25 +1,123 @@
 <template lang="pug">
 .main
   img.title(src="/images/6-video-title.png")
-  .box
-    input#filepicker.uploadVideo_input(type="file", accept="video/mp4")
-    video(
-      src="",
-      poster="images/6-video-btn.png",
-      webkit-playsinline="true",
-      playsinline="true",
-      x-webkit-airplay="allow",
-      x5-video-player-type="h5"
+  video.preview(
+    preload="metadata",
+    src="",
+    v-show="selectedVideo",
+    @click="playPause",
+    ref="video"
+  )
+  .box(v-show="!selectedVideo")
+    img.img(src="/images/6-video-btn.png")
+    input#upload(
+      type="file",
+      name="file",
+      accept="video/*",
+      value="",
+      ref="file-input",
+      @change="onFileChange"
     )
   .tips
-    | 视频视频视频视频视频视频视频视频视频视频视频视频视频视频视频视频视频视频
-  .btn.flexCenter 提交
+    | {{ day.requirement }}
+  .btn.flexCenter(
+    @click="submit",
+    :class="{ disabled: !selectedVideo || submitting }",
+    v-if="!success"
+  )
+    span(v-if="submitting") 提交中…
+    span(v-else-if="uploading") 上传中 {{ (uploadProgress * 100).toFixed() }}% …
+    span(v-else) 提交
+  answer-success(v-if="success")
 </template>
+
+<script>
+import AnswerSuccess from "../components/AnswerSuccess";
+import { get100Day, saveAnswer, http } from "@/helpers/resource";
+
+export default {
+  components: { AnswerSuccess },
+  data: () => {
+    return {
+      day: {},
+      videoUrl: "",
+      selectedVideo: false,
+      preview: "",
+      success: false,
+      uploading: false,
+      submitting: false,
+      uploadProgress: 0,
+    };
+  },
+  methods: {
+    async submit() {
+      if (!this.videoUrl) return;
+      this.submitting = true;
+      await saveAnswer(this.$openid, this.day.id, { answer: this.videoUrl });
+      this.submitting = false;
+      this.success = true;
+    },
+    onFileChange(e) {
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createVideo(files[0]);
+    },
+    createVideo(file) {
+      const video = this.$refs["video"];
+      video.src = URL.createObjectURL(file);
+      this.selectedVideo = true;
+      // let reader = new FileReader();
+      // let vm = this;
+
+      // reader.onload = (e) => {
+      //   vm.preview = e.target.result;
+      // };
+      // reader.readAsDataURL(file);
+      this.uploadImage(file);
+    },
+    async uploadImage(file) {
+      const formData = new FormData();
+      formData.append("photo", file);
+      this.uploading = true;
+      const fileObject = (
+        await http.post("pbpark/photo", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) =>
+            (this.uploadProgress = progressEvent.loaded / progressEvent.total),
+        })
+      ).data;
+      this.uploading = false;
+      this.videoUrl = fileObject.url;
+    },
+    removeVideo() {
+      if (!confirm("确定删除图片重新上传？")) return;
+      this.preview = "";
+      this.videoUrl = "";
+      this.$emit("input", null);
+      this.$refs["file-input"].value = "";
+    },
+    playPause() {
+      const video = this.$refs["video"];
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    },
+  },
+  async created() {
+    const dayId = this.$route.query.dayId;
+    this.day = await get100Day(dayId);
+  },
+};
+</script>
 
 <style scoped>
 .main {
   width: 100%;
-  height: 21.16rem;
+  /* height: 21.16rem; */
   background: url(/images/3-bg.png) no-repeat;
   background-size: cover;
   background-position: center center;
@@ -38,12 +136,12 @@
 }
 video {
   width: 100%;
-  height: 100%;
-  object-fit: fill;
+  object-fit: cover;
   display: block;
+  margin: 0 auto;
   position: absolute;
-  left: 0;
-  top: 0;
+  height: calc(100% - 3.75rem);
+  object-position: center;
 }
 .box img {
   position: absolute;
@@ -69,18 +167,21 @@ video {
   position: absolute;
   left: 50%;
   margin-left: -2.35rem;
-  top: 18.3rem;
+  bottom: 1rem;
   font-size: 0.6rem;
   font-weight: bold;
   color: #956134;
 }
-.uploadVideo_input {
-  position: absolute;
+.btn.disabled {
+  opacity: 0.5;
+}
+#upload {
   width: 100%;
   height: 100%;
-  left: 0;
+  position: absolute;
   top: 0;
-  opacity: 0;
+  left: 0;
   z-index: 2;
+  opacity: 0;
 }
 </style>
