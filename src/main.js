3a402af99e6f3a7df0179e1383ecda9349c28d1c
Uice Lu <uicestone@gmail.com>
2021-04-05 22:43:36 +0800

    wechat auth user info

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ Vue.config.productionTip = false;
 Vue.use(globalVars);
 
 (async () => {
-  const result = await wechatRedirect();
+  const result = await wechatRedirect(true);
   result &&
     new Vue({
       router,
