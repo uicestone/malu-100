91553addeb33b9fa67c0b2d6d1af8fd37525052f
Uice Lu <uicestone@gmail.com>
2021-05-01 00:02:00 +0800

    use https url for backend

--- src/helpers/resource.js
+++ src/helpers/resource.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const http = Axios.create({
-  baseURL: "http://pbpark.hbird.com.cn/wp-json/v1",
+  baseURL: "https://pbpark.hbird.com.cn/wp-json/v1",
 });
 
 export async function getPosts(params) {
